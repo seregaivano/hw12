11adf0f83444ef325acd1a75e4a67afc978be1ee
Superuser <sergeyivanov2x1@gmail.com>
2023-12-04 14:01:32 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Author Turgenev = new Author("Иван", "Тургенев");
 
+        Book fathersAndSon = new Book("Отцы и дети", 5555, Turgenev);
+
+        Author Feynman = new Author("Иван", "Фейман");
+
+        Book kED = new Book("Квантовая электролинамика", 1000, Feynman);
+
+        fathersAndSon.setPublisherYear(6666);
+        kED.setPublisherYear(2000);
+        System.out.println(fathersAndSon);
+        System.out.println(kED);
     }
 }
+
+
+
